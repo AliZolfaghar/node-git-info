ebdd1da43e832d7fc5eded39e16d8bde3fef9469
ali zolfaghar <azolfaghar@gmail.com>
2021-11-23 10:46:34 +0330

    1400/08/01

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-var getRepoInfo = require('git-repo-info');
+// var getRepoInfo = require('git-repo-info');
  
-var info = getRepoInfo();
+// var info = getRepoInfo();
  
-console.log(info)
+// console.log(info)
+
+
+var exec = require('child_process').exec;
+
+function run(command, callback){
+    exec(command, function(error, stdout, stderr){ 
+        console.log('#ERROR' , error)
+        console.log('#ERROR' , stderr)
+        callback(stdout); 
+    });
+};
+
+
+const cmd = "git fetch && git show-ref";
+
+run(cmd,(result)=>{
+    // console.log('execute command : ' , cmd);
+    var data = result.split('\n')
+                .map(item => item.split(' ')[0])
+                .filter(item => item.length > 0 )
+
+
+    console.log(data)
+})
